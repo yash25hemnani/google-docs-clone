1a855f12752f7f671abb196caacfd9e62edaa6e8
yash25hemnani <yashhemnanigit@gmail.com>
2025-09-11 13:23:45 +0530

    TipTap Setup Done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex min-h-screen items-center justify-center text-blue-500 bg-red-500'>
+      Hello world
+    </div>
+  )
+}
+
+export default Home
